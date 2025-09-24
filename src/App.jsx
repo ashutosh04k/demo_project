f37cf02bda9a020b8ff2b83207b2b9c2317b43a1
ashutosh04k <kumarasutosh2014@gmail.com>
2025-09-24 21:18:49 +0530

    number update

--- src/App.jsx
+++ src/App.jsx
@@ -349,7 +349,7 @@ export default function App() {
               <div className="mt-4 space-y-2 text-sm text-gray-400">
                 <div>📍 Rasis Business Center Office #77, </div>
                 <div style={{ marginLeft: '18px' }}>3rd Floor –Al Barsha – Dubai – United Arab Emirates</div>
-                {/* <div>✉️ rohan@shadesandmotion.com</div> */}
+                <div>✉️ Creative@creativebay.ae</div>
                 {/* <div style={{ marginLeft: '15px' }}>sparlee@shadesandmotion.com</div> */}
                 <div>📞 +971 502 826 450</div>
                 <div style={{ marginLeft: '18px' }}> +971 547 958 431</div>
@@ -380,7 +380,7 @@ export default function App() {
 
                 <div className="sm:col-span-2 flex items-center justify-between">
                   <button type="submit" className="px-4 py-2 rounded-lg bg-indigo-600 text-white">Send message</button>
-                  {/* <small className="text-xs text-gray-500"> Or email us at rohan@shadesandmotion.com</small> */}
+                  <small className="text-xs text-gray-500"> Or email us at Creative@creativebay.ae</small>
                 </div>
               </form>
             </div>
